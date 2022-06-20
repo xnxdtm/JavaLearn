1145c6d67475dec5d26bc0645bd5cb7dc1671af9
xnxdtm <xnxdtm@163.com>
2022-06-20 17:46:57 +0800

    [second] 解决异常

--- dongbao-application/dongbao-portal-web/src/main/java/com/dongbao/portal/web/PortalWebApplication.java
+++ dongbao-application/dongbao-portal-web/src/main/java/com/dongbao/portal/web/PortalWebApplication.java
@@ -1,4 +1,5 @@
-package com.dongbao.ums;
+package com.dongbao.portal.web;
+
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -6,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.dongbao"})
 @MapperScan("com.dongbao.ums.mapper.xml")
-public class DongbaoUmsApplication {
+public class PortalWebApplication{
 
     public static void main(String[] args) {
-        SpringApplication.run(DongbaoUmsApplication.class, args);
+        SpringApplication.run(PortalWebApplication.class, args);
     }
 
 }
---
+++ dongbao-service/dongbao-ums/src/test/java/com/dongbao/ums/UmsServiceTest.java
@@ -0,0 +1,24 @@
+package com.dongbao.ums;
+
+import com.dongbao.ums.service.UmsMemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UmsServiceTest {
+
+    @Autowired
+    UmsMemberService umsMemberService;
+
+    @Test
+    void testServer(){
+        umsMemberService.hello();
+    }
+
+    @Test
+    void testRegister() {
+        umsMemberService.register();
+    }
+
+}
