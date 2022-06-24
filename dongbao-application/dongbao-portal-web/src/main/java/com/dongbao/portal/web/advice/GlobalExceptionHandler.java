86c17d32ecc48287b6f3c462febec6578473f1fa
xnxdtm <xnxdtm@gmail.com>
2022-06-24 08:26:49 +0800

    [important] Kaptcha及结合redis实现验证码

--- dongbao-application/dongbao-portal-web/src/main/java/com/dongbao/portal/web/advice/GlobalExceptionHandler.java
+++ dongbao-application/dongbao-portal-web/src/main/java/com/dongbao/portal/web/advice/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.dongbao.portal.web.advice;
 
+import com.baomidou.kaptcha.exception.KaptchaException;
+import com.baomidou.kaptcha.exception.KaptchaIncorrectException;
+import com.baomidou.kaptcha.exception.KaptchaNotFoundException;
+import com.baomidou.kaptcha.exception.KaptchaTimeoutException;
 import com.dongbao.common.base.result.ResultWrapper;
 import com.dongbao.portal.web.exception.TokenException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,5 +26,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(TokenException.class)
     public ResultWrapper<Object> loginException(Exception e) {
         return ResultWrapper.getFailBuilder().msg(e.getMessage()).build();
+    }
+
+    @ExceptionHandler(KaptchaException.class)
+    public String kcaptchaException(KaptchaException e){
+        if (e instanceof KaptchaTimeoutException){
+            return "超时";
+        }else if (e instanceof KaptchaIncorrectException){
+            return "不正确";
+        }else if (e instanceof KaptchaNotFoundException){
+            return "没找到";
+        }else {
+            return "反正错了";
+        }
     }
 }
