8e26e788425a81092efb169bc9685e7c0677b293
xnxdtm <xnxdtm@163.com>
2022-08-16 19:34:01 +0800

    发布订阅模型的消息队列

---
+++ RabbitMQ/consumer/src/main/java/com/rabbitmq/ConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wujing
+ * @version V1.0
+ * @Description
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class);
+    }
+}
