d9db793478d5bd419099dcc935dad31ff5f3a258
xnxdtm <xnxdtm@163.com>
2022-08-23 17:26:52 +0800

    nacos 服务者消费者

---
+++ springCloudAlibaba/nacosProvider2/src/main/java/com/nacos/NacosApplication.java
@@ -0,0 +1,27 @@
+package com.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class, args);
+    }
+
+    @RestController
+    class EchoController {
+        @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+        public String echo(@PathVariable String string) {
+            return "1 Hello Nacos Discovery " + string;
+        }
+    }
+
+}
